c559e5b8e420898ff9619f65410a9d89eb3394eb
bobo008 <1030831926@qq.com>
2022-12-08 11:03:10 +0800

    aa

---
+++ thbtest/Classes/ttthod.h
@@ -0,0 +1,16 @@
+//
+//  ttthod.h
+//  PuppetPic
+//
+//  Created by tanghongbo on 2022/12/8.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface ttthod : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ thbtest/Classes/ttthod.m
@@ -0,0 +1,12 @@
+//
+//  ttthod.m
+//  PuppetPic
+//
+//  Created by tanghongbo on 2022/12/8.
+//
+
+#import "ttthod.h"
+
+@implementation ttthod
+
+@end
